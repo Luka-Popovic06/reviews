7c02b4f0415b798ce2eaa84d4a11c5fa60ea7436
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-04 20:41:44 +0200

    Add personsInfo data array

---
+++ src/info.js
@@ -0,0 +1,34 @@
+export const personsInfo = [
+  {
+    id: 1,
+    src: "person-1.jpeg",
+    firstName: "Susan ",
+    lastName: "Smith",
+    job: "web developer",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    src: "person-2.jpeg",
+    firstName: "Anna ",
+    lastName: "Johnson",
+    job: "web designer",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    src: "person-3.jpeg",
+    firstName: "Bill ",
+    lastName: "Anderson",
+    job: "the boss",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+  },
+  {
+    id: 4,
+    src: "person-4.jpeg",
+    firstName: "Peter",
+    lastName: "Jones",
+    job: "intern",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+];
